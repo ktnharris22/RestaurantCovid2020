d137628436520273d49133c147a7bfad854078f5
Jaih <jhunterh@andrew.cmu.edu>
2020-11-29 15:00:00 -0700

    UpdatedCodefor Dhruva_JAHH

--- Soup_JAHH.py
+++ Soup_JAHH.py
@@ -17,17 +17,24 @@ import numpy as np
 
 #def createDF(): df=pd.DataFrame(columns = ['Name', 'Address', 'Posted on','Removed on','Violation'])return df
 
+#Function that clips desired data from string of data and returns string
+#Calls clean function with removes line breaks and unnecessary spaces at start or ned of string.
+def clip(stringData,startTag, endTag):
+    sIndex = stringData.find(startTag)
+    eIndex = stringData.find(endTag)
+    temp = stringData[sIndex + len(startTag):eIndex]
+    clippedData = cleanUp(temp)
+    return clippedData
 
-#Function that cleans data strings given a pre-set list of "fillers" 
+#Function that cleans data strings given a pre-set list of "fillers"
 def cleanUp (stringData):
-    newData = ''
-    
+    newData = stringData
     fillerZ = ['\r\n']
-    
     for i in range(len(fillerZ)):
-        lenFiller = len(fillerZ[i])
-        cleanIndex = stringData.find(fillerZ[i])
-        newData = stringData[0:cleanIndex+lenFiller].strip() + ' ' + stringData[cleanIndex+lenFiller:].strip()
+        cleanIndex = newData.find(fillerZ[i])
+        if cleanIndex != -1: #checks to see if the specific filler is a part of the substring of data
+            lenFiller = len(fillerZ[i])
+            newData = newData[0:cleanIndex+lenFiller].strip() + ' ' + newData[cleanIndex+lenFiller:].strip()
     return newData #type string
 
 #Function that takes in beautifulSouplList and parses data into a usable data struct for further processing
@@ -35,35 +42,47 @@ def parseData(beautifulSoupList):
     
     #creating data parsing tags for searching string
     headerKeyWord = 'NOTE: ' #checks for non-restuarant data
-    tagRestName = '<tr>\n<td style="cursor: default;">'
     
-    tagViol = 'Violation:<br/>' #violation tag
-    lenTagViol = len(tagViol)
+    tagName_s = '<tr>\n<td style="cursor: default;">'
+    tagName_e = '<br/>'
     
-    tagPost = 'Posted: '
-    lenTagPost = len(tagPost)
+    tagAdd_s = '<br/>'
+    tagAdd_e = '<br/>' #check to see if they are the same
+    
+    tagZip_s = 'PA'
+    tagZip_e = '<p>'
+    
+    tagBoro_s = '<p>'
+    tagBoro_e = '</p>'
+    
+    tagPDate_s = 'Posted: '
+    tagpDate_e = '<br/>'
+    
+    tagPRem_s = 'Removed:'
+    tagPRem_e = '</p>'
+    
+    tagViol_s = 'Violation:<br/>' #violation tag
+    tagViol_e = '</p>\n</td>\n</tr>'
+        
+    tagParamList = [['NAME',tagName_s,tagName_e],['ADDRESS',tagAdd_s,tagAdd_e], ['ZIP',tagZip_s,tagZip_e], ['BORO',tagBoro_s, tagBoro_e],['POST',tagPDate_s,tagpDate_e],['Remove',tagPRem_s,tagPRem_e],['VIOLATION',tagViol_s,tagViol_e]]
 
-    tagRemove = 'Removed: '
-    lenTagRemove = len(tagRemove)
-    
-    tagEnd = '</p>\n</td>\n</tr>'
-    lenTagEnd = len(tagEnd)
-    
-    
     for i in range(len(beautifulSoupList)):
         rawParameter = str(beautifulSoupList[i])
+        #checking to ensure that the rawparameter isn't heading information
         if rawParameter.find(headerKeyWord) == -1:
-            
-            violIndex = rawParameter.find(tagViol)
-            endIndex = rawParameter.find(tagEnd)
-            temp = rawParameter[violIndex+lenTagViol:endIndex]
-            rawParameter = cleanUp(rawParameter)
-    
+            for j in range(len(tagParamList)):
+                startT = tagParamList[j][1]
+                endT = tagParamList[j][2]
+                temp = clip(rawParameter,startT,endT)
+                
+                #Dhruva to create desired structure to input into larger data frame
+                #tagParamList[j][0] 'Name' key
+                #temp 'Name Data'
+                
+                
     
     dataStruct = dict()
     
-
-
 def main():
     
     #URL 
