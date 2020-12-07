6e68336eccb7aa7ed3871630021f06fb066aa628
Javier <jasrico@gmail.com>
2020-12-07 00:32:23 -0500

    Tables joined

--- app.py
+++ app.py
@@ -113,6 +113,33 @@ def geolocateAddress(address):
 # address_df.to_csv('AddressesLongLat.csv', index=False, header=True)
 
 
+#--------JARA
+map_lat_long = pd.read_csv("AddressesLongLat.csv")
+map_lat_long = map_lat_long[map_lat_long['Longitude'] > -82]
+col = map_lat_long.Address.str.split(expand=True)
+col[3] = col[3].map(lambda x: x.lstrip(',').rstrip(','))
+col.rename(columns={0:"num",3:"city",4:"state"},inplace=True)
+col["street"] = col[1]+" "+col[2]
+col[["latitude","longitude","address"]] = map_lat_long[["Latitude","Longitude","Address"]]
+col.drop(col.columns[[1,2,5,6,7,8]],axis=1,inplace=True)
+
+map_df = pd.read_csv("RestaurantInspect.csv")
+map_df = map_df[map_df['city'] == 'Pittsburgh']
+map_df["entireaddress"]=map_df["num"]+map_df["street"]+map_df["city"]
+map_df["entireaddress"]=map_df["entireaddress"].str.replace(" ","")
+col["entireaddress"]=col["num"]+col["street"]+col["city"]
+col["entireaddress"] = col["entireaddress"].str.replace(" ","")
+
+map_df_1 = pd.merge(map_df, col, left_on='entireaddress', right_on='entireaddress',how='left')
+
+map_df_4=pd.DataFrame()
+map_df_4[['facility_name', 'longitude', 'latitude', 'address']] = \
+    map_df_1[['facility_name', 'longitude', 'latitude', 'address']]
+map_df_4[["street address"]] = map_df_1["num_x"] + ' ' + map_df_1["street_x"] + " Pittsburgh, PA "
+map_df_4=map_df_4[pd.notnull(map_df_4.longitude)]
+map_df_4=map_df_4[pd.notnull(map_df_4.facility_name)]
+###-------JARA
+
 #%%ProcessGeoFoodFacs.csv
 category_cd_list = [117, 118, 201, 202, 203, 211, 212, 250, 407]  # relevant categeory codes
 cutoff = datetime.datetime(2018, 1, 1)  # cutoff threshold for recency
@@ -128,6 +155,13 @@ geo_df['street address'] = geo_df['num'] + ' ' + geo_df['street'] + ' Pittsburgh
 geo_df = geo_df.drop(columns=['num', 'street', 'city', 'state', 'municipal', 'category_cd', 'description', 'p_code', 'fdo',
                               'bus_st_date', 'bus_cl_date', 'seat_count', 'noroom', 'sq_feet', 'status', 'placard_st', 'status', 'zip'])
 geo_valid = geo_df[geo_df['longitude'].notna()]
+
+#-------JARA
+geo_valid = geo_valid.drop(columns=['id'])
+geo_valid = geo_valid.append(map_df_4,ignore_index = True)
+#-------JARA
+
+
 merge_df = pd.merge(final_map_df, geo_valid, on='street address', how='left')
 
 
